9016e92da50578e347843dcdc64f6f3cc9cd07eb
Basil M. <basil@mail.com>
2026-07-11 21:35:48 +0300

    Задание 2: Reviewer оценивает студентов, Student оценивает лекторов :) вот прикол

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -22,6 +22,20 @@ class Student:
         else:
             return 'Ошибка'
 
+    # новый метод - студент может оценить лекцию
+    def rate_lecture(self, lecturer, course, grade):
+        """Метод для выставления оценки лектору за лекцию."""
+        # проверяем что оценим именно лектора, а не кого попало
+        if isinstance(lecturer, Lecturer) and \
+                course in self.courses_in_progress and \
+                course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     """Базовый класс для преподавателей."""
@@ -32,13 +46,11 @@ class Mentor:
         self.courses_attached = []
 
 
-# теперь Mentor - родительский класс, от него наследуем
 class Lecturer(Mentor):
     """Класс лектора. Наследуется от Mentor."""
 
     def __init__(self, name, surname):
         super().__init__(name, surname)
-        # у лектора будут хранить оценки за его лекции
         self.grades = {}
 
 
@@ -48,12 +60,33 @@ class Reviewer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
 
+    # метод rate_hw теперь только у Reviewer
+    def rate_hw(self, student, course, grade):
+        """Метод для выставления оценки студенту за домашнее задание."""
+        if isinstance(student, Student) and \
+                course in self.courses_attached and \
+                course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
-# проверка что наследование работает
+
+# проверяем как работает
 lecturer = Lecturer('Иван', 'Иванов')
 reviewer = Reviewer('Пётр', 'Петров')
+student = Student('Алёхина', 'Ольга', 'Ж')
 
-print(isinstance(lecturer, Mentor))  # True
-print(isinstance(reviewer, Mentor))  # True
-print(lecturer.courses_attached)     # []
-print(reviewer.courses_attached)     # []
+student.courses_in_progress += ['Python', 'Java']
+lecturer.courses_attached += ['Python', 'C++']
+reviewer.courses_attached += ['Python', 'C++']
+
+# студент оценивает лекции
+print(student.rate_lecture(lecturer, 'Python', 7))  # None - ок
+print(student.rate_lecture(lecturer, 'Java', 8))    # Ошибка - лектор не ведёт Java
+print(student.rate_lecture(lecturer, 'C++', 8))     # Ошибка - студент не изучает C++
+print(student.rate_lecture(reviewer, 'Python', 6))  # Ошибка - это не лектор
+
+print(lecturer.grades)  # {'Python': [7]}
